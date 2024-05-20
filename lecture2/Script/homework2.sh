9ad21317e46a86a4a5bb8069260ce54c340ee4f5
Huyha <hagiahuy311@gmail.com>
2024-05-21 00:27:44 +0700

    commit

--- lecture2/Script/homework2.sh
+++
@@ -1,9 +0,0 @@
-for name in `ls /home/huyha/`; do
-if [[ -f "/home/huyha/$name" ]];then
-    echo "$name is file"
-elif [[ -d "/home/huyha/$name"  ]];then
-    echo "$name is directory"
-else
-    echo "$name is orther type"
-fi
-done
--- lecture2/Script/question1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-num1=5
-num2=10
-num3=$(($num1+$num2))
-
-echo $(($num1 + $num2 - $num3))
-echo $num3"
-echo " $1 % $num3" | bc
