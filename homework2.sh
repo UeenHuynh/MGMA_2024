b570ae4d3143eb64f8c9d4daf5e7daf3df659ea0
Huyha <hagiahuy311@gmail.com>
2024-05-30 19:29:42 +0700

    Add files via upload

---
+++ homework2.sh
@@ -0,0 +1,9 @@
+for name in `ls /home/huyha/`; do
+if [[ -f "/home/huyha/$name" ]];then
+    echo "$name is file"
+elif [[ -d "/home/huyha/$name"  ]];then
+    echo "$name is directory"
+else
+    echo "$name is orther type"
+fi
+done
