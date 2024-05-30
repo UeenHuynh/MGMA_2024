4209361e2f7e6fb965e4439d2ce2d7b8ca4de021
duy-dk <khuongduying@gmail.com>
2024-05-30 19:11:40 +0700

    update lecture 4 practice code

---
+++ lecture4/practice/code/practice.sh
@@ -0,0 +1,12 @@
+
+# Download data
+curl https://mothur.s3.us-east-2.amazonaws.com/data/MiSeqDevelopmentData/StabilityNoMetaG.tar -o StabilityNoMetaG.tar
+
+
+# read per sample
+for sample in `ls *.gz | cut -d "_" -f1 | uniq`; do
+   n_read=`zcat ${sample}_*.gz | grep "^@M" | wc -l`;
+   echo -e $sample '\t' $n_read;
+done > ../out/n_read_per_sample.tsv
+
+sort -nk2 ../out/read_count.tsv
