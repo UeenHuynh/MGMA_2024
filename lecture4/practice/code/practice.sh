86b603467410d3381b4a94e062cbe44edfec430a
duy-dk <khuongduying@gmail.com>
2024-05-30 19:14:33 +0700

    extract tar file

--- lecture4/practice/code/practice.sh
+++ lecture4/practice/code/practice.sh
@@ -2,6 +2,7 @@
 # Download data
 curl https://mothur.s3.us-east-2.amazonaws.com/data/MiSeqDevelopmentData/StabilityNoMetaG.tar -o StabilityNoMetaG.tar
 
+tar -xvf StabilityNoMetaG.tar
 
 # read per sample
 for sample in `ls *.gz | cut -d "_" -f1 | uniq`; do
