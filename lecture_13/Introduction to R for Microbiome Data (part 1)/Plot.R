e9c430a25d06bc6e35fbaa7910c7a741a9f7f565
VanHoHoangKim <168974162+VanHoHoangKim@users.noreply.github.com>
2024-07-01 05:22:35 +0700

    Update and rename Boxplot_TH.R to Plot.R

--- lecture_13/Introduction to R for Microbiome Data (part 1)/Plot.R
+++ lecture_13/Introduction to R for Microbiome Data (part 1)/Plot.R
@@ -1,5 +1,3 @@
-library(readxl)
-library(ggplot2)
 library(ggpubr)
 
 data_PT <- read_excel("C:/Users/Kim/Downloads/BT/plateassay_growthrates.xlsx")
@@ -50,3 +48,49 @@ p_3 <- ggboxplot(data_PT, x = "photoperiod", y = "mumax",
                  add = "jitter", shape = "photoperiod")
 combined_plot <- ggarrange(p_2, p_3,  ncol = 2, nrow = 1,
                            common.legend = FALSE, legend = "bottom")
+# Specify the pairwise group comparisons
+comps <- list(c("4", "9"), c("4", "13.5"), c("9", "13.5"))
+
+# Plot 1: Violin plot with boxplot inside, standard p-value labels
+plot1 <- ggviolin(data_PT, x = "temperature", y = "mumax", fill = "temperature",
+                  palette = c("#bcbddc", "#fa9fb5", "#abcdef"),
+                  add = "boxplot", add.params = list(fill = "yellow")) +
+  stat_compare_means(comparisons = comps) +
+  stat_compare_means(label.y = 1.30)
+
+
+# Plot 2: Violin plot with boxplot inside, customized p-value labels
+plot2 <- ggviolin(data_PT, x = "temperature", y = "mumax", fill = "temperature",
+                  palette = c("#bcbddc", "#fa9fb5", "#abcdef"),
+                  add = "boxplot", add.params = list(fill = "yellow")) +
+  stat_compare_means(comparisons = comps, label = "p.signif") + 
+  stat_compare_means(label.y = 1.30)
+
+# Combine the plots into a single frame
+ggarrange(plot1, plot2, ncol = 2, nrow = 1,
+          common.legend = TRUE, legend = "bottom")
+
+
+data_PT$temperature <- factor(data_PT$temperature)
+p_4 <- ggdensity(data_PT, x = "mumax",
+          add = "mean", rug = TRUE,
+          color = "temperature", fill = "temperature",
+          palette = c("#feb24c", "#c51b8a", "#2b8cbe"))
+
+data_PT$temperature <- factor(data_PT$temperature)
+p_5 <- ggdensity(data_PT, x = "mumax",
+          add = "mean", rug = TRUE,
+          color = "temperature", fill = "temperature",
+          palette = "jco")
+
+p_6 <- gghistogram(data_PT, x = "mumax",
+            add = "mean", rug = TRUE,
+            color = "temperature", fill = "temperature",
+            bins = 20,
+            palette = c("#c994c7", "#fec44f", "#2c7fb8"))
+
+p_7 <- gghistogram(data_PT, x = "mumax",
+        add = "mean", rug = TRUE,
+        color = "temperature", fill = "temperature",
+        bins = 30,
+        palette = c("#c994c7", "#fec44f", "#2c7fb8"))
